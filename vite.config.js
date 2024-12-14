f896dd88a7b73a3db2379d0d715c59d513022909
Nani230 <naturalstarnani230@gmail.com>
2024-12-14 12:44:54 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "@components": "/src/components",
+            "@components": path.resolve(__dirname, "src/components"),
             "@pages": "/src/pages",
             "@animation": "/src/animation",
             "@assets": "/src/assets",
